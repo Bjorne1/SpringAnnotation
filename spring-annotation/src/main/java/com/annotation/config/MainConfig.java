3dd90cdac92755d2ac9a87070c1969ba6d1325bb
Bjorne1 <35253647+Bjorne1@users.noreply.github.com>
2019-01-22 16:02:58 +0800

    add ComponentScan setting

--- spring-annotation/src/main/java/com/annotation/config/MainConfig.java
+++ spring-annotation/src/main/java/com/annotation/config/MainConfig.java
@@ -1,9 +1,8 @@
 package com.annotation.config;
 
 import com.annotation.bean.Person;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
+import org.springframework.stereotype.Controller;
 
 /**
  * @Description: Configuration声明这是一个配置类
@@ -11,7 +10,13 @@ import org.springframework.context.annotation.Configuration;
  * @Date: Created in 2019/1/22 15:17
  */
 @Configuration
-@ComponentScan(basePackages = "com.annotation")//指定要扫描的参数,还有exclude,include等
+@ComponentScans(
+        value = {
+                @ComponentScan(basePackages = "com.annotation", includeFilters = {
+                        @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class}),
+                }, useDefaultFilters = false) //使用includeFilters时,要设置useDefaultFilters = false,其默认是true
+        }
+)
 public class MainConfig {
 
     /**
